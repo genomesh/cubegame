ebc3df979ed972057128505f2dcb5fdbab08117c
dragon.maxen@gmail.com <dragon.maxen@gmail.com>
2017-10-24 18:13:39 +0100

    bug fixes

--- app.js
+++ app.js
@@ -9,17 +9,44 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-let highscores = [];
-for (let i = 0;i<10;i++) {
-    highscores[i] = [];
-    for (let j = 0;j<3;j++) {
-        highscores[i][j] = {
-            name : "NONE",
-            score : 0
-        }
-    }
-}
+const { Client } = require('pg')
 
+const client = new Client({
+  connectionString: 'postgres://vidcrhaaiunhxj:bd4a1113af6f63752a510e92b411c0ca9470a8bdd3c8b0470f693a792165355d@ec2-54-235-88-58.compute-1.amazonaws.com:5432/d4t3892f1qnivf',
+  ssl: true
+})
+
+let fromDB = [];
+let highscores = [];
+
+client.connect();
+console.log('connected');
+
+client.query('SELECT * FROM highscores;', (err, res) => {
+  if (err) throw err;
+  console.log(res.rows.length);
+  fromDB = [];
+  for (let row of res.rows) {
+    fromDB[fromDB.length] = row;
+  }
+  highscores = createArray(fromDB);
+  client.end();
+  console.log('disconnected');
+});
+
+function createArray (i) {
+    return i.reduce((acc, cur) => {
+        if (acc[cur.level] == undefined) {
+            acc[cur.level] = [];
+        }
+        acc[cur.level][cur.id] = {
+            name:cur.name,
+            score:cur.score
+        };
+        
+        return acc;
+    }, []);
+}
 let hits = 0;
 
 app.use(bodyParser.text());
@@ -51,6 +78,9 @@ app.post('/highscore',function (req, res) {
         })
     }
     highscores[theLevel].length=3;
+    if (theScore > highscores[theLevel][2].score) {
+        //send new highscore to DB
+    }
     res.setHeader('Content-Type', 'application/json');
     res.json(highscores);
 })
